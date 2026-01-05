c32b17faf2a71aae25950f0df318d8eaabce678d
unknown <sarvarbekakramjonov51@gmail.com>
2026-01-05 22:09:40 +0500

    O'zgartirishlar kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,19 @@ const App = () => {
 	};
 
 	const onSendData = useCallback(() => {
-		telegram.sendData(JSON.stringify(cartItems));
+		const queryID = telegram.initDataUnsave?.query_id;
+
+		if (queryID) {
+			fetch("http://localhost:8000/web-data", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(cartItems),
+			});
+		} else {
+			telegram.sendData(JSON.stringify(cartItems));
+		}
 	}, [cartItems]);
 
 	useEffect(() => {
